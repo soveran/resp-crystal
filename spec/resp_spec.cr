d59031a9fb6173475ab7a69cf7794df6124d3a5a
Michel Martens <michel@soveran.com>
2019-10-27 16:02:43 +0100

    Fix CI integration

--- spec/resp_spec.cr
+++ spec/resp_spec.cr
@@ -2,8 +2,9 @@ require "./spec_helper"
 
 describe "Resp" do
   before do
-    c = Resp.new("redis://localhost:#{REDIS_PORT}")
-    c.call("FLUSHDB")
+    connect("redis://#{REDIS_HOST}:#{REDIS_PORT}") do |c|
+      c.call("FLUSHDB")
+    end
   end
 
   it "should encode expressions as RESP" do
@@ -11,8 +12,9 @@ describe "Resp" do
   end
 
   it "should accept host and port" do
-    c = Resp.new("redis://localhost:#{REDIS_PORT}")
-    assert_equal "tcp_port:#{REDIS_PORT}", info(c, "server")["tcp_port:#{REDIS_PORT}"]
+    connect("redis://#{REDIS_HOST}:#{REDIS_PORT}") do |c|
+      assert_equal "tcp_port:#{REDIS_PORT}", info(c, "server")["tcp_port:#{REDIS_PORT}"]
+    end
   end
 
   it "should accept auth" do
@@ -20,7 +22,9 @@ describe "Resp" do
     ex = nil
 
     begin
-      Resp.new("redis://:foo@localhost:#{REDIS_PORT}")
+      connect("redis://:foo@#{REDIS_HOST}:#{REDIS_PORT}") do |c|
+        c.ping
+      end
       raise Exception.new
     rescue ex : Resp::Error
       assert_equal message, ex.message
@@ -28,93 +32,98 @@ describe "Resp" do
   end
 
   it "should accept db" do
-    c = Resp.new("redis://localhost:#{REDIS_PORT}/3")
+    connect("redis://#{REDIS_HOST}:#{REDIS_PORT}/3") do |c|
+      c.call("SET", "foo", "1")
+      assert_equal 1, c.call("DBSIZE")
 
-    c.call("SET", "foo", "1")
-    assert_equal 1, c.call("DBSIZE")
-
-    c.call("SELECT", "0")
-    assert_equal 0, c.call("DBSIZE")
+      c.call("SELECT", "0")
+      assert_equal 0, c.call("DBSIZE")
+    end
   end
 
   it "should accept a URI without a path" do
-    c = Resp.new("redis://localhost:#{REDIS_PORT}")
-    assert_equal "tcp_port:#{REDIS_PORT}", info(c, "server")["tcp_port:#{REDIS_PORT}"]
+    connect("redis://#{REDIS_HOST}:#{REDIS_PORT}") do |c|
+      assert_equal "tcp_port:#{REDIS_PORT}", info(c, "server")["tcp_port:#{REDIS_PORT}"]
+    end
   end
 
   it "should accept a URI with an empty path" do
-    c = Resp.new("redis://localhost:#{REDIS_PORT}/")
-    assert_equal "tcp_port:#{REDIS_PORT}", info(c, "server")["tcp_port:#{REDIS_PORT}"]
+    connect("redis://#{REDIS_HOST}:#{REDIS_PORT}/") do |c|
+      assert_equal "tcp_port:#{REDIS_PORT}", info(c, "server")["tcp_port:#{REDIS_PORT}"]
+    end
   end
 
   it "should accept a URI with a numeric path" do
-    c = Resp.new("redis://localhost:#{REDIS_PORT}/3")
-    assert_equal "tcp_port:#{REDIS_PORT}", info(c, "server")["tcp_port:#{REDIS_PORT}"]
+    connect("redis://#{REDIS_HOST}:#{REDIS_PORT}/3") do |c|
+      assert_equal "tcp_port:#{REDIS_PORT}", info(c, "server")["tcp_port:#{REDIS_PORT}"]
+    end
   end
 
   it "should accept a URI with an invalid path" do
-    c = Resp.new("redis://localhost:#{REDIS_PORT}/a")
-    assert_equal "tcp_port:#{REDIS_PORT}", info(c, "server")["tcp_port:#{REDIS_PORT}"]
+    connect("redis://#{REDIS_HOST}:#{REDIS_PORT}/a") do |c|
+      assert_equal "tcp_port:#{REDIS_PORT}", info(c, "server")["tcp_port:#{REDIS_PORT}"]
+    end
   end
 
   it "should accept commands" do
-    c = Resp.new("redis://localhost:#{REDIS_PORT}")
-
-    assert_equal "PONG", c.call("PING")
+    connect("redis://#{REDIS_HOST}:#{REDIS_PORT}") do |c|
+      assert_equal "PONG", c.call("PING")
+    end
   end
 
   it "should accept arrays as commands" do
-    c = Resp.new("redis://localhost:#{REDIS_PORT}")
-
-    assert_equal "PONG", c.call(["PING"])
+    connect("redis://#{REDIS_HOST}:#{REDIS_PORT}") do |c|
+      assert_equal "PONG", c.call(["PING"])
+    end
   end
 
   it "should accept non-string arguments" do
-    c = Resp.new("redis://localhost:#{REDIS_PORT}")
-
-    assert_equal "OK", c.call("SET", "foo", 1)
-    assert_equal "1",  c.call("GET", "foo")
+    connect("redis://#{REDIS_HOST}:#{REDIS_PORT}") do |c|
+      assert_equal "OK", c.call("SET", "foo", 1)
+      assert_equal "1",  c.call("GET", "foo")
+    end
   end
 
   it "should pipeline commands" do
-    c = Resp.new("redis://localhost:#{REDIS_PORT}")
+    connect("redis://#{REDIS_HOST}:#{REDIS_PORT}") do |c|
+      c.queue("ECHO", "hello")
+      c.queue("ECHO", "world")
 
-    c.queue("ECHO", "hello")
-    c.queue("ECHO", "world")
-
-    assert_equal ["hello", "world"], c.commit
+      assert_equal ["hello", "world"], c.commit
+    end
   end
 
   it "should raise Redis errors" do
-    c = Resp.new("redis://localhost:#{REDIS_PORT}")
+    connect("redis://#{REDIS_HOST}:#{REDIS_PORT}") do |c|
+      assert_raise(Resp::Error) do
+        c.call("FOO")
+      end
 
-    assert_raise(Resp::Error) do
-      c.call("FOO")
-    end
+      c.queue("SET", "foo", "42")
+      c.queue("SMEMBERS", "foo")
+      c.queue("HGETALL", "foo")
 
-    c.queue("SET", "foo", "42")
-    c.queue("SMEMBERS", "foo")
-    c.queue("HGETALL", "foo")
-
-    assert_raise(Resp::Error) do
-      c.commit
+      assert_raise(Resp::Error) do
+        c.commit
+      end
     end
   end
 
   it "should be able to get the URL back from the client" do
-    c = Resp.new("redis://localhost:#{REDIS_PORT}/8")
-    assert_equal "redis://localhost:#{REDIS_PORT}/8", c.url
+    connect("redis://#{REDIS_HOST}:#{REDIS_PORT}/8") do |c|
+      assert_equal "redis://#{REDIS_HOST}:#{REDIS_PORT}/8", c.url
+    end
   end
 
   it "should accept missing methods as commands" do
-    c = Resp.new("redis://localhost:#{REDIS_PORT}")
-
-    assert_equal "PONG", c.ping
-    assert_equal "OK", c.set("foo", "42")
-    assert_equal "42", c.get("foo")
-    
-    assert_raise(Resp::Error) do
-      c.foo
+    connect("redis://#{REDIS_HOST}:#{REDIS_PORT}") do |c|
+      assert_equal "PONG", c.ping
+      assert_equal "OK", c.set("foo", "42")
+      assert_equal "42", c.get("foo")
+      
+      assert_raise(Resp::Error) do
+        c.foo
+      end
     end
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,8 +1,23 @@
 require "crotest"
 require "../src/resp"
 
+REDIS_HOST = ENV.fetch("REDIS_HOST", "localhost")
 REDIS_PORT = ENV.fetch("REDIS_PORT", "6379")
 
 def info(c : Resp, section = "default")
   c.call("INFO", section).as String
+end
+
+def connect(url : String)
+  c = Resp.new(url)
+  yield c
+ensure
+  disconnect(c)
+end
+
+def disconnect(c : Nil)
+end
+
+def disconnect(c : Resp)
+  c.finalize
 end
